32678d08509b235d9a6739b72ede0ac9dbce02d4
FantasyFiend <522381613@qq.com>
2017-07-23 22:53:40 +0800

    '发布'

--- src/main/java/cn/siwangyin/ServiceCenter.java
+++ src/main/java/cn/siwangyin/ServiceCenter.java
@@ -2,6 +2,8 @@ package cn.siwangyin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -9,7 +11,12 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class ServiceCenter {
+public class ServiceCenter extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ServiceCenter.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceCenter.class, args);
